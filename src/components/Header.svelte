85f4ad14f31636e488f67eecb01b53d389c78416
constraintAutomaton <Vous@exemple.com>
2025-05-29 11:32:29 +0200

    first

---
+++ src/components/Header.svelte
@@ -0,0 +1,20 @@
+<div class="header">
+    <div>
+            <h1><a href="./">Constraint Automaton</a></h1>
+    </div>
+    <div>
+        <a href="./blog">Blog</a>
+        <a href="./project">Projects</a>
+        <a href="./publication">Publication</a>
+        <a href="./resume">Resume</a>
+    </div>
+</div>
+
+<style>
+    .header{
+        display: flex;
+        flex-direction: column;
+        width: 100%;
+        align-items: center;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Header from '../components/Header.svelte';
+</script>
+
+<div class="content">
+    <Header/>
+    <p>
+        I'm a PhD student in computer science working on the topic of decentralized querying.
+        I'm also very interesting in logical programming (Prolog mainly 🙂).
+        You can find most of my work on <a href="https://github.com/constraintAutomaton">Github</a>
+    </p>
+</div>
+
+<style>
+    .content{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
