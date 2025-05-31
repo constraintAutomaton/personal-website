8a49288133f46fcc135b5fb085d5a43e41836c4c
constraintAutomaton <Vous@exemple.com>
2025-05-31 20:33:39 +0200

    publication page

---
+++ src/routes/publication/Publication.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { type IPublication } from '$lib';
+	let { authors, year, venue, url, title }: IPublication = $props();
+	const stringAuthors = authors.join(' and ');
+</script>
+
+<div class="publication">
+	<div><span>Title:</span> <span><a href={url}>{title}</a></span></div>
+	<div><span>Author:</span> <span>{stringAuthors}</span></div>
+	<div><span>Year:</span> <span>{year}</span></div>
+	<div><span>Venue:</span> <span>{venue}</span></div>
+</div>
+
+<style>
+	.publication {
+		display: flex;
+		flex-direction: column;
+		margin-bottom: 1vh;
+		border-bottom: solid 1px;
+	}
+</style>
