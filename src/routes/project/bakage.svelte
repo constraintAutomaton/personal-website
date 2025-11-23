2665e1e254b785c65a08e3fafb13bed86e6a04d5
constraintAutomaton <Vous@exemple.com>
2025-11-23 07:17:47 +0100

    webpage updated

--- src/routes/project/bakage.svelte
+++ src/routes/project/bakage.svelte
@@ -3,5 +3,13 @@
 	<p>
 		<q cite="https://github.com/bakaq/bakage">An experimental package manager for Scryer Prolog.</q>
 		Contributed to enhancing dependency fetching and developing a lockfile system to improve reproducibility.
+		This work was presented at
+		<a href="https://hsd-pbsa.de/veranstaltung/scryer-prolog-meetup-2025/"
+			>The 3rd Scryer Prolog Meetup</a
+		>
+		and is
+		<a href="https://github.com/constraintAutomaton/bakage-scryer-prolog-meetup-presentation"
+			>available online here</a
+		>.
 	</p>
 </div>
